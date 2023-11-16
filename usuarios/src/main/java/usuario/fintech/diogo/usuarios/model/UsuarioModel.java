fc73d4f2c52c1a7a8f3ed68b1ef9ad0052bdc284
Diogo Paza <diogopazacvel@gmail.com>
2023-11-15 21:42:50 -0300

    service discovery e gateway

---
+++ usuarios/src/main/java/usuario/fintech/diogo/usuarios/model/UsuarioModel.java
@@ -0,0 +1,9 @@
+package usuario.fintech.diogo.usuarios.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class UsuarioModel {
+    
+    
+}
---
+++ usuarios/src/test/java/usuario/fintech/diogo/usuarios/UsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package usuario.fintech.diogo.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
